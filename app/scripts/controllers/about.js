42abff7b77e2d2cb000c01cec71e4fda866e6f43
special <github@specialorange.org>
2016-02-21 19:14:16 -0700

    initial commit with bower, grunt, ng, and node

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name highTideAviationApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the highTideAviationApp
+ */
+angular.module('highTideAviationApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name highTideAviationApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the highTideAviationApp
+ */
+angular.module('highTideAviationApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
